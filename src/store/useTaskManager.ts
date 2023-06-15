4e108c1ed96a9b846a2495c9708ee13c4428c95b
fetraandri <hei.fetra@gmail.com>
2023-06-15 11:32:39 +0300

    feat: add store with zustand

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,41 @@
-const useTaskManager = () => {
+import create from 'zustand';
 
-}
 
-export {
-  useTaskManager
-}
+// Création du store Zustand pour gérer les tâches
+const useTaskStore = create((set) => ({
+  tasks: [],
+
+  // Recherche d'une tâche par titre
+  searchTask: (title: string) => {
+    set((state: { tasks: any[]; }) => ({
+      tasks: state.tasks.filter((task: { title: string; }) =>
+        task.title.toLowerCase().includes(title.toLowerCase())
+      ),
+    }));
+  },
+
+  // Ajout d'une nouvelle tâche
+  addTask: (newTask: any) => {
+    set((state: { tasks: any; }) => ({
+      tasks: [...state.tasks, newTask],
+    }));
+  },
+
+  // Mise à jour des détails d'une tâche existante par titre
+  updateTask: (title: any, updatedTask: any) => {
+    set((state: { tasks: any[]; }) => ({
+      tasks: state.tasks.map((task: { title: any; }) =>
+        task.title === title ? { ...task, ...updatedTask } : task
+      ),
+    }));
+  },
+
+  // Suppression d'une tâche par titre
+  deleteTask: (title: any) => {
+    set((state: { tasks: any[]; }) => ({
+      tasks: state.tasks.filter((task: { title: any; }) => task.title !== title),
+    }));
+  },
+}));
+
+export default useTaskStore;
